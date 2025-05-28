cc18061b07d0ccf93a8362415f7415500512973d
malex1106 <malex1106@gmail.com>
2025-05-28 14:43:42 +0200

    Update READMEs and add naming to models in experiments script

--- beat_this/run_exp.sh
+++ beat_this/run_exp.sh
@@ -5,11 +5,11 @@ BOOM_NOISE=40
 BOOM_NUM=6
 
 for seed in 0 1 2; do
-    python launch_scripts/train.py --seed "$seed" --max-epochs "$MAX_EPOCHS" --no-val --no-tempo-augmentation --no-pitch-augmentation --no-mask-augmentation --no-boom-augmentation
+    python launch_scripts/train.py --seed "$seed" --max-epochs "$MAX_EPOCHS" --no-val --no-tempo-augmentation --no-pitch-augmentation --no-mask-augmentation --no-boom-augmentation --logger "wandb" --name "No-augmentation-$seed"
 
-    python launch_scripts/train.py --seed "$seed" --max-epochs "$MAX_EPOCHS" --no-val --no-boom-augmentation
+    python launch_scripts/train.py --seed "$seed" --max-epochs "$MAX_EPOCHS" --no-val --no-boom-augmentation --logger "wandb" --name "Pitch-tempo-mask-$seed"
 
-    python launch_scripts/train.py --seed "$seed" --max-epochs "$MAX_EPOCHS" --no-val --no-tempo-augmentation --no-pitch-augmentation --no-mask-augmentation --boom-noise "$BOOM_NOISE" --boom-num "$BOOM_NUM"
+    python launch_scripts/train.py --seed "$seed" --max-epochs "$MAX_EPOCHS" --no-val --no-tempo-augmentation --no-pitch-augmentation --no-mask-augmentation --boom-noise "$BOOM_NOISE" --boom-num "$BOOM_NUM" --logger "wandb" --name "Boomerang-sampling-$seed"
 
-    python launch_scripts/train.py --seed "$seed" --max-epochs "$MAX_EPOCHS" --no-val --boom-noise "$BOOM_NOISE" --boom-num "$BOOM_NUM"
+    python launch_scripts/train.py --seed "$seed" --max-epochs "$MAX_EPOCHS" --no-val --boom-noise "$BOOM_NOISE" --boom-num "$BOOM_NUM" --logger "wandb" --name "All-augmentations-$seed"
 done
